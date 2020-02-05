759ad40b7912cbef938ab8e06f3217c02e50d52c
raj kumar gu8pta <rajkumargupta731@gmail.com>
2020-02-05 22:00:27 +0545

    added

---
+++ react-chat-client/src/App.js
@@ -0,0 +1,17 @@
+// src/App.js
+
+import React, { Component } from 'react';
+// import './App.css'; <-- commented out for styling
+import ConversationsList from './components/ConversationsList';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ConversationsList />
+      </div>
+    );
+  }
+}
+
+export default App;
